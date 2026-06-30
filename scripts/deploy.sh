b7412d63f1af706e3cc803bd18345d8e88cec2f5
link <1641324393@qq.com>
2026-06-30 10:40:00 +0800

    chore: support local monorepo deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,14 +3,12 @@ set -eu
 
 ROOT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
 
-: "${TYPECHO_HOST:?Set TYPECHO_HOST, for example TYPECHO_HOST=example.com}"
 : "${TYPECHO_ROOT:?Set TYPECHO_ROOT, for example TYPECHO_ROOT=/www/wwwroot/example.com/typecho}"
 
 TYPECHO_USER="${TYPECHO_USER:-ubuntu}"
 SSH_OPTS="${SSH_OPTS:-}"
 RSYNC_OPTS="${RSYNC_OPTS:--az --delete}"
 
-REMOTE="${TYPECHO_USER}@${TYPECHO_HOST}"
 PLUGIN_TARGET="${TYPECHO_ROOT}/usr/plugins/TypechoPay/"
 THEME_TARGET="${TYPECHO_ROOT}/usr/themes/VOID/"
 
@@ -23,10 +21,21 @@ EXCLUDES="
 --exclude '*.log'
 "
 
-rsync ${RSYNC_OPTS} ${EXCLUDES} -e "ssh ${SSH_OPTS}" \
-  "${ROOT_DIR}/plugins/TypechoPay/" "${REMOTE}:${PLUGIN_TARGET}"
+if [ -n "${TYPECHO_HOST:-}" ]; then
+  REMOTE="${TYPECHO_USER}@${TYPECHO_HOST}"
+  rsync ${RSYNC_OPTS} ${EXCLUDES} -e "ssh ${SSH_OPTS}" \
+    "${ROOT_DIR}/plugins/TypechoPay/" "${REMOTE}:${PLUGIN_TARGET}"
 
-rsync ${RSYNC_OPTS} ${EXCLUDES} -e "ssh ${SSH_OPTS}" \
-  "${ROOT_DIR}/themes/VOID/" "${REMOTE}:${THEME_TARGET}"
+  rsync ${RSYNC_OPTS} ${EXCLUDES} -e "ssh ${SSH_OPTS}" \
+    "${ROOT_DIR}/themes/VOID/" "${REMOTE}:${THEME_TARGET}"
 
-echo "Deployed TypechoPay and VOID to ${REMOTE}:${TYPECHO_ROOT}"
+  echo "Deployed TypechoPay and VOID to ${REMOTE}:${TYPECHO_ROOT}"
+else
+  rsync ${RSYNC_OPTS} ${EXCLUDES} \
+    "${ROOT_DIR}/plugins/TypechoPay/" "${PLUGIN_TARGET}"
+
+  rsync ${RSYNC_OPTS} ${EXCLUDES} \
+    "${ROOT_DIR}/themes/VOID/" "${THEME_TARGET}"
+
+  echo "Deployed TypechoPay and VOID to ${TYPECHO_ROOT}"
+fi
